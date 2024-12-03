c978d0c0eeff1b4a10ccca79a83907d7506532ab
Alexander Pislegin <pislegin.as@phystech.edu>
2024-12-04 01:04:18 +0300

    home assignment 6

--- 6.cpp
+++ 6.cpp
@@ -1,104 +1,177 @@
 #include<iostream>
 #include<string>
+#include<map>
+#include<typeinfo>
 
-class Animal {
-    friend class Vet;
+class SpellBook;
 
-    private:
-        int health = 0;
-
-    public:
-        unsigned age;
-        std::string name;
-        std::string breed;
-
-        void data()
-        {
-            std::cout << "Age: " << age << "\nName: " << name << "\nBreed: " << breed << "\n";
-        }
-
-        virtual void makeSound(){}
-
-        Animal(unsigned page, std::string pname, std::string pbreed) {
-            name = pname;
-            age = page;
-            breed = pbreed;
-        }
+class Spell{
+public:
+    std::string name;
+    std::string description;
+    virtual void readSpell(){}
+    Spell(std::string pname, std::string desc) : name(pname), description(desc) {}
 };
 
-class Dog: public Animal {
-    public:
-        void makeSound() override{
-            std::cout << "borf\n";
-        }
+class Wizard;
+class Warlock;
+class Herbalist;
+class Domovoy;
 
-        Dog(unsigned page, std::string pname, std::string pbreed): Animal(page, pname, pbreed){}
+class WaterSpell : public Spell{
+    WaterSpell(std::string pname, std::string desc) : Spell(pname, desc) {}
+    void readSpell() override{
+        std::cout << "Water spell '" << name << "': " << description << "\n";
+    }
+    friend Warlock;
+    friend Herbalist;
+    friend Domovoy;
 };
 
-class Cat: public Animal {
-    public:
-        void makeSound() override{
-            std::cout << "You betrayed Christ.\n";
-        }
-
-        Cat(unsigned page, std::string pname, std::string pbreed): Animal(page, pname, pbreed){}
+class EarthSpell : public Spell{
+    EarthSpell(std::string pname, std::string desc) : Spell(pname, desc) {}
+    void readSpell() override{
+        std::cout << "Earth spell '" << name << "': " << description << "\n";
+    }
+    friend Warlock;
+    friend Herbalist;
+    friend Domovoy;
 };
 
-class Owner {
-    public:
-        unsigned age;
-        std::string fio;
-        unsigned n_pets;
-        Animal* pets;
-
-        Owner(unsigned page, std::string pfio, unsigned pn_pets, Animal* ppets) {
-            age = page;
-            fio = pfio;
-            n_pets = pn_pets;
-            this->pets = ppets;
-        }
-
-        void data()
-        {
-            std::cout << "Age: " << age << "\nFio: " << fio << "\nNumber of pets: " << n_pets << "\n";
-        }
+class FireSpell : public Spell{
+    FireSpell(std::string pname, std::string desc) : Spell(pname, desc) {}
+    void readSpell() override{
+        std::cout << "Fire spell '" << name << "': " << description << "\n";
+    }
+    friend Warlock;
+    friend Herbalist;
+    friend Domovoy;
 };
 
-class Vet {
-    public:
-        std::string fio;
-    
-        void talk(Owner subject) {
-            std::cout << fio << " talks with " << subject.fio << ".\n";
-        }
-
-        void healthCheck(Animal subject) {
-            std::cout << subject.name << "'s health is " << subject.health << ". (Diagnosed by " << fio << ").\n";
-        }
-
-        void heal(Animal &subject) {
-            std::cout << fio << " heals " << subject.name << " for 3!\n";
-            subject.health += 3;
-        }
-
-        Vet(std::string pfio) {
-            fio = pfio;
-        }
+class AirSpell : public Spell{
+    AirSpell(std::string pname, std::string desc) : Spell(pname, desc) {}
+    void readSpell() override{
+        std::cout << "Air spell '" << name << "': " << description << "\n";
+    }
+    friend Warlock;
+    friend Herbalist;
+    friend Domovoy;
 };
 
-int main()
-{
-    Dog subject1{3, "Johny", "Samoyed"};
-    Cat subject2{0, "Orion", "Derevensky"};
-    subject1.data();
-    subject1.makeSound();
-    subject2.data();
-    subject2.makeSound();
-    Animal pets[]{subject1, subject2};
-    Owner human{19, "Vasilevskii Alexander Alexeevich", 2, pets};
-    Vet doctor{"Vasileva Tatiana Ivanovna"};
-    doctor.healthCheck(subject2);
-    doctor.heal(subject2);
-    doctor.healthCheck(subject2);
-    doctor.talk(human);
+class SpellBook{
+    std::map<std::string, Spell*> spells;
+    public:
+    void writeSpell(Spell& sp){
+        if(spells.contains(sp.name)){
+            throw std::out_of_range("SpellAlreadyWritten");
+        }
+        spells[sp.name] = &sp;
+    }
+    void eraseSpell(std::string nam){
+        if(!spells.contains(nam)){
+            throw std::out_of_range("SpellNotFound");
+        }
+        spells.erase(nam);
+    }
+    void readSpell(std::string nam){
+        if(!spells.contains(nam)){
+            throw std::out_of_range("SpellNotFound");
+        }
+        (*spells[nam]).readSpell();
+    }
+    friend Wizard;
+};
+
+class Wizard{
+    public:
+    virtual WaterSpell createWaterSpell() = 0;
+    virtual EarthSpell createEarthSpell() = 0;
+    virtual FireSpell createFireSpell() = 0;
+    virtual AirSpell createAirSpell() = 0;
+};
+
+class Warlock : public Wizard{
+    public:
+    const std::string type = "Warlock";
+    virtual WaterSpell createWaterSpell() override{
+        return WaterSpell("Evaporation", "Thy enemy's eyes shalt be lacking humidity.");
+    }
+    virtual EarthSpell createEarthSpell() override {
+        return EarthSpell("Quicksand", "Maketh the ground tremble beneath vile folk.");
+    }
+    virtual FireSpell createFireSpell() override {
+        return FireSpell("Fire arrow", "Launcheth a swift missile of flames.");
+    }
+    virtual AirSpell createAirSpell() override {
+        return AirSpell("Whirlwind", "A fierce storm appears at thy will.");
+    }
+    template<typename T>
+    friend T callWizard();
+};
+
+class Herbalist : public Wizard{
+    public:
+    const std::string type = "Herbalist";
+    virtual WaterSpell createWaterSpell() override{
+        return WaterSpell("Summon: water", "A mighty water elemental shalt be thoust guard.");
+    }
+    virtual EarthSpell createEarthSpell() override {
+        return EarthSpell("Skin Petrification", "Your flesh becomes resilient to any harm.");
+    }
+    virtual FireSpell createFireSpell() override {
+        return FireSpell("Firewall", "None treacherous vermin shalt pass through!");
+    }
+    virtual AirSpell createAirSpell() override {
+        return AirSpell("Discombobulate", "Striketh enemy's ears with a shockwave to stun them.");
+    }
+    template<typename T>
+    friend T callWizard();
+};
+
+class Domovoy : public Wizard{
+    public:
+    const std::string type = "Domovoy";
+    virtual WaterSpell createWaterSpell() override{
+        return WaterSpell("Aquatic creation", "Fill a small vial with pure water.");
+    }
+    virtual EarthSpell createEarthSpell() override {
+        return EarthSpell("Pottery", "An intricate clay vessel is brought into existence.");
+    }
+    virtual FireSpell createFireSpell() override {
+        return FireSpell("Bonfire", "Light a piece of wood on fire.");
+    }
+    virtual AirSpell createAirSpell() override {
+        return AirSpell("Aerial affinity","A light breeze enhances thoust swiftness and dexterity.");
+    }
+    template<typename T>
+    friend T callWizard();
+};
+
+template<typename T>
+T callWizard(){
+    std::cout << "A " << T().type << " is called to arms!\n";
+    return T();
+}
+
+int main(){
+    Warlock Merlin = callWizard<Warlock>();
+    EarthSpell spell1 = Merlin.createEarthSpell();
+    AirSpell spell2 = Merlin.createAirSpell();
+    Domovoy Kuzya = callWizard<Domovoy>();
+    AirSpell spell3 = Kuzya.createAirSpell();
+    SpellBook book;
+    try{
+        book.writeSpell(spell1);
+        book.readSpell("Quicksand");
+        book.writeSpell(spell2);
+        //book.writeSpell(spell2);
+        book.readSpell("Whirlwind");
+        book.writeSpell(spell3);
+        book.readSpell("Aerial affinity");
+        book.eraseSpell("Aerial affinity");
+        //book.readSpell("Aerial Affinity");
+    }
+    catch(std::out_of_range& s) {
+        std::cout << "Exception: " << s.what() << "!\n";
+    }
 }
